e906b9b6d4411b2579f74b3cc30b8fcbe7f234df
wiraardy <seng14141@gmail.com>
2024-06-01 13:57:13 +0700

    add button view password & forget password

--- assets/js/login.js
+++ assets/js/login.js
@@ -125,7 +125,8 @@
       showToast('bg-danger', 'Error!', 'Something went wrong!\n' + error.message);
     });
   });
-  function addEventListeners() {
+  
+        function addEventListeners() {
             document.getElementById('loginWithEmail').addEventListener('click', function () {
                 document.getElementById('whatsapp-login-form').style.display = 'none';
                 document.getElementById('email-login-form').style.display = 'block';
@@ -142,6 +143,16 @@
                     event.preventDefault();
                     alert('Please complete the CAPTCHA');
                 }
+            });
+
+            document.getElementById('togglePassword').addEventListener('click', function (e) {
+                // Toggle the type attribute
+                const password = document.getElementById('password');
+                const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+                password.setAttribute('type', type);
+                // Toggle the eye icon
+                this.classList.toggle('bx-hide');
+                this.classList.toggle('bx-show');
             });
         }
 
