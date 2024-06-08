75e9fe362362ac269884a5fbd9994f150b59b9c1
Dwi Rizqi <a.dwirizqi.h123@gmail.com>
2024-06-08 11:05:46 +0700

    change price

--- assets/js/constants/dataRedeem.js
+++ assets/js/constants/dataRedeem.js
@@ -1,18 +1,18 @@
 const mapRedeemItem = {
     'Money': {
         icon: 'bx bx-money bx-sm text-danger me-3',
-        price: 10000
+        price: 200000000
     },
     'Limit': {
         icon: 'bx bx-shape-circle bx-sm text-info me-3',
-        price: 20000
+        price: 200000
     },
     'XP': {
         icon: 'bx bx-infinite bx-sm text-success me-3',
-        price: 30000
+        price: 180000000
     },
     'Double XP': {
         icon: 'bx bx-radio-circle-marked bx-sm text-primary me-3',
-        price: 40000
+        price: 8000000000000
     }
 }
